3d6a2acdd20e75925114eba8cabc5cd450b0e192
Diego Serrano <diego.s.ibarra@gmail.com>
2026-05-17 19:53:02 -0500

    fix: add site and base for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://dserranoi.github.io',
+  base: '/elite-autodetail',
   integrations: [react()],
   vite: {
     plugins: [tailwindcss()],
